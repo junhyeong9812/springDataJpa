b3777921853c2d7226ee7fe7dd1626aeef805f6e
jun <pickjog@gmail.com>
2024-09-29 22:43:43 +0900

    SpringJpaData Interface test

--- src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/data_jpa/repository/MemberRepositoryTest.java
@@ -8,6 +8,7 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.data_jpa.entity.Member;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -39,4 +40,48 @@ class MemberRepositoryTest {
 
 
     }
+
+    @Test
+    public void bassicCRUD(){
+        Member member1=new Member("member1");
+        Member member2=new Member("member2");
+        memberRepository.save(member1);
+        memberRepository.save(member2);
+
+        //단건 조회 검증
+        Member findMember1 = memberRepository.findById(member1.getId()).get();
+        Member findMember2 = memberRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        //유저 업데이트 확인
+        findMember1.setUsername("member!!!!!!");
+        //DB 쿼리 결과
+//        AGE  	MEMBER_ID  	TEAM_ID  	USERNAME
+//        0	    1	        null	member!!!!!!
+//        0	    2	        null	member2
+//        이렇게 이름이 변경된 것을 확인할 수 있다.
+//        2024-09-28T21:38:23.244+09:00  INFO 17396 --- [data-jpa] [           main] p6spy                                    : #1727527103244 | took 0ms | statement | connection 3| url jdbc:h2:tcp://localhost/~/datajpa
+//        update member set age=?,team_id=?,username=? where member_id=?
+//        update member set age=0,team_id=NULL,username='member!!!!!!' where member_id=1;
+//        이렇게 쿼리로 update문이 나가는 것을 알 수 있다.
+
+        //리스트 조회 검증
+        List<Member> all = memberRepository.findAll();
+        assertThat(all.size()).isEqualTo(2);
+
+        //카운트 검증
+        long count = memberRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        //삭제 검증
+        memberRepository.delete(member1);
+        memberRepository.delete(member2);
+
+        long deletedCount = memberRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
+
+    }
+    //이렇게 인터페이스로 구현체로 만들어도 정상적으로 동작하는 것을 확인할 수 있다.
+
 }
